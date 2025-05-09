85f91a1f438772386f968dcb7af7f4c9e2626ef2
高欣雨 <3136199363@qq.com>
2025-05-10 01:50:06 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # my_list = [1, 2, 3, 4, 5]
+    # return render_template("index.html", my_list=my_list)
+    return render_template("child.html")
